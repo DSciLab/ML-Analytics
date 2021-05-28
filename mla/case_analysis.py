80624e41dcc04fd8b6b54fbfb57ef5f8510f747d
tor4z <vwenjie@hotmail.com>
2021-05-28 20:18:11 +0800

    init

---
+++ mla/case_analysis.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class CaseAnalysis(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def set_metric(self, metric):
+        pass
+
+
+class SuccessCaseAnalysis(CaseAnalysis):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class FailureCaseAnalysis(CaseAnalysis):
+    def __init__(self) -> None:
+        super().__init__()
+
